5b9786179844cb40ed91133da78bcf869e59e0b6
DeLaTorreLaraArturo <arturo.delato@hotmail.com>
2017-10-18 11:01:23 -0500

    Excepciones basicas

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,7 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exceptions {
 
-    public static void main(String[] arg)
+    public static void main(String[] arg) throws FileNotFoundException
     {
-        System.out.println("hola");
+        try {
+            File archivo = new File("entrada.txt");
+            Scanner teclado = new Scanner(archivo);
+            System.out.print("Dime un numero: ");
+            int num1 = teclado.nextInt();
+            System.out.print("Dime otro numero: ");
+            int num2 = teclado.nextInt();
+            System.out.print("La division es:  " + num1 / num2);
+        }
+        catch(ArithmeticException expecionAritmetica)
+        {
+            System.out.println("Division entre cero");
+        }
+        catch(InputMismatchException excepcionEntrada)
+        {
+            System.out.println("Entrada Incorrecta");
+        }
+       /* catch(FileNotFoundException e)
+        {
+            System.out.println("No existe el archivo");
+        }*/
+
     }
 }
